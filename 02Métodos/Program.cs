a40d8cf72c3e3ee957fea309ed0d7c0106b3b74a
Andre Santos <andresantosas2558487@gmail.com>
2021-12-28 14:33:48 -0300

    first commit

---
+++ 02Métodos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02Métodos
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // Instanciando objeto
+
+            Pessoa obj = new Pessoa();
+            obj.apresentar("Pedro", 32);
+        }
+    }
+}
